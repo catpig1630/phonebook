c7f0b9ba425547eb84efe41ff388cb90758118cd
catpig1630 <f74031043@mail.ncku.edu.tw>
2018-03-12 13:23:42 +0800

    Decrease entry size

--- phonebook_opt.h
+++ phonebook_opt.h
@@ -5,9 +5,9 @@
 
 /* TODO: After modifying the original version, uncomment the following
  * line to set OPT properly */
-// #define OPT 1
+#define OPT 1
 typedef struct __PHONE_BOOK_ENTRY {
-    char lastName[MAX_LAST_NAME_SIZE];
+    //char lastName[MAX_LAST_NAME_SIZE];
     char firstName[16];
     char email[16];
     char phone[10];
@@ -17,7 +17,13 @@ typedef struct __PHONE_BOOK_ENTRY {
     char city[16];
     char state[2];
     char zip[5];
-    struct __PHONE_BOOK_ENTRY *pNext;
+    //struct __PHONE_BOOK_ENTRY *pNext;
+} entry_all;
+
+typedef struct __LASTNAME_ENTRY {
+    char lastName[MAX_LAST_NAME_SIZE];
+    struct __LASTNAME_ENTRY *pNext;
+    entry_all *all;
 } entry;
 
 entry *findName(char lastName[], entry *pHead);
